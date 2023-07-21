be0626f880146f4b55da6d663072395030d617c7
tomko69 <110322510+tomko69@users.noreply.github.com>
2023-07-21 11:34:50 +0800

    ndjs

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5500;
+
+app.use(cors());
+
+const TodoItemsRoute = require('./routes/todoItems');
+
+mongoose.connect(process.env.DB_CONNECT)
+.then(()=>console.log("Database connected"))
+.catch(err => console.log(err))
+
+app.use('/', TodoItemsRoute);
+
+
+
+app.listen(PORT, () => console.log("server connected"));
